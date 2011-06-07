65488510992bf1fed03cd1bed3fb5ffdb2505fe6
Stefan Richter <stefan@02strich.de>
2011-06-07 14:49:01 +0200

    add new internet radio plugin

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ MPD_PORT   = 6600
 MPD_HOST   = "localhost"
 HTTP_PORT  = 8080
 TAG_FORMAT = "<b>%(artist)s</b> - %(title)s"
+COUCHDB_URL = ""
---
+++ plugins/iradio/__init__.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#      wymypy.py
+#
+#      Copyright 2007 Marc Lentz <manatlan@gmail.com>
+#
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+import base64
+u64enc = base64.urlsafe_b64encode
+u64dec = base64.urlsafe_b64decode
+
+from couchdbkit import *
+
+import config
+
+class Greeting(Document):
+    url = StringProperty()
+
+class IRadio(wPlugin):
+    def init(self):
+        self.button_index = 50
+        
+        self.server = Server(uri = config.COUCHDB_URL)
+        self.db = self.server.get_or_create_db("mpd_radio")
+    
+    def show(self):
+        return """
+            <button onclick='ajax_radio()'>Radio</button>
+        """
+    
+    def ajax_radio(self, ignore = 0, rsrnd = 0):
+        yield "<h2>Internet Radio</h2>"
+        
+        # list stations
+        index = 0
+        for i in self.db.all_docs().all():
+            cur = self.db.get(i['id'])
+            classe = index % 2 == 0 and " class='p'" or ''
+            yield "<li%s>" % classe
+            yield """<a href='#' onclick='ajax_streamPlay("%s");'><span>></span></a>""" % (u64enc(cur['url']),)
+            yield cur['url']
+            yield "</li>"
+            index += 1
+        
+        # add new station
+        yield "<h3>Add Internet Station:"
+        yield """
+          <form onsubmit='ajax_streamAdd($("radio").value);return false'>
+            <input type='text' id='radio' size="10"/>
+            <button type='submit'>add</button>
+          </form>
+        """
+    
+    def ajax_streamPlay(self, url, rsrnd = 0):
+        f = u64dec(url)
+        print f
+        self.mpd.add([f])
+        return "player"
+    
+    def ajax_streamAdd(self, f, rsrnd = 0):
+        self.db.save_doc({'url': f})
+        for ele in self.ajax_radio():
+            yield ele
+        #return "player"
+    
+
