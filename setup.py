28e57168602236df4f9043bd8886022931b244e0
Stefan Richter <stefan@02strich.de>
2011-06-25 20:38:37 +0200

    renaming it for pypi - only the original author can use the wymypy name there

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='wymypy',
+setup(name='wymypy-ng',
     version = '2.0',
     description = 'Simple web interface for controlling MPD',
     maintainer = 'Stefan Richter',
     maintainer_email = 'stefan@02strich.de',
+    url = 'http://www.02strich.de/',
     packages = find_packages(),
     package_data = {'wymypy': ['static/*.*', 'templates/*.*']},
     install_requires = ['Flask'],
